a68b09a4e9bf4650358e48a309b5e94d3e73b8b3
Anindow <anindowsaikat@gmail.com>
2023-04-17 16:41:04 +0600

    going home task unfinished

---
+++ nextjs-course-code/pages/components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+const Button = (props) => {
+  return (
+    <Link href={props.link}>
+      <div className={classes.btn}>{props.children}</div>
+    </Link>
+  );
+};
+
+export default Button;
