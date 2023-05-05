f96c32b81523208d3eedfdc258960158e001def2
Anindow <anindowsaikat@gmail.com>
2023-05-06 02:02:02 +0600

    working on njs-course destruckturing the paths

--- Njx-course/pages/[pid].js
+++ Njx-course/pages/[pid].js
@@ -2,9 +2,9 @@ import path from "path"
 import fs from "fs/promises"
 function ProductDetailPage(props) {
   const { loadedProduct } = props
-//   if(!loadedProduct){
-//     return <p>Loading...</p>
-//   }
+  //   if(!loadedProduct){
+  //     return <p>Loading...</p>
+  //   }
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -12,13 +12,18 @@ function ProductDetailPage(props) {
     </>
   )
 }
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json")
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+  return data
+}
 
 export async function getStaticProps(context) {
   const { params } = context
   const productId = params.pid
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json")
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
+
+  const data = await getData()
 
   const product = data.products.find((product) => product.id === productId)
   return {
@@ -28,14 +33,13 @@ export async function getStaticProps(context) {
   }
 }
 export async function getStaticPaths() {
-  return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
+    const data = await getData()
+    const ids = data.products.map(product => product.id)
+    const pathsWithParams = ids.map(id => ({params: {pid:id}}))
+    return {
+    paths:   pathsWithParams ,
     // fallback: true,
-    fallback: "blocking",    
+    fallback: "blocking",
     // if we use this we dont need the if (Loading ...) condition
   }
 }
